d63931b303bdf3f77feb27c4c76ea42cb70dfce0
patrick d. <p1e85@users.noreply.github.com>
2026-03-25 19:37:25 -0500

    Update ThemeManager.js

--- amigapad/js/classes/ThemeManager.js
+++ amigapad/js/classes/ThemeManager.js
@@ -1,15 +1,17 @@
 // js/classes/ThemeManager.js
 // Manages Light (Workbench 1.3) vs Dark theme
 
+import { StorageManager } from './StorageManager.js';
+
 export class ThemeManager {
     constructor() {
         this.isDark = false;
-        this.applyTheme();
+        this.loadSavedTheme();
     }
 
     toggle() {
         this.isDark = !this.isDark;
-        StorageManager.saveTheme(this.isDark); // We'll import StorageManager later
+        StorageManager.saveTheme(this.isDark);
         this.applyTheme();
     }
 
@@ -22,7 +24,6 @@ export class ThemeManager {
             root.classList.remove('dark-theme');
         }
         
-        // Future: could dispatch a custom event for other components to react
         console.log(`Amiga Pad theme: ${this.isDark ? 'Dark' : 'Workbench 1.3 Light'}`);
     }
 
